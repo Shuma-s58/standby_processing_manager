2938efb6c04b7611a941ff58fdc7d1b561192655
Shuma-s58 <shuma.suzuki.2021@outlook.jp>
2024-11-06 09:28:48 +0000

    add rectangle_and_circle_marker_node.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'rectangle_and_circle_marker_node = standby_processing_manager.rectangle_and_circle_marker_node:main',
         ],
     },
 )
